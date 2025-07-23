8ca8974f26e4f255d40f069dae5da2131054c35b
aijaz <saijazahmad38@gmail.com>
2025-07-23 19:44:14 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit} from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/next";
 
 const outfit = Outfit({
   subsets: ["latin"],
